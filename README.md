525d1500dea6294906308f64cccac56963e1aab8
Jeremy <jereamon@gmail.com>
2019-06-22 12:04:37 -0700

    Added an individual donations template and route for it in main.py.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Flask Mailroom Application
 
-Running at [http://afternoon-reef-51666.herokuapp.com/donations/](http://afternoon-reef-51666.herokuapp.com/donations/).
+Running at [http://morning-sierra-61743.herokuapp.com/donations/].
 
 ## Your Task
 
--- main.py
+++ main.py
@@ -35,6 +35,23 @@ def new_donation():
         return redirect(url_for('all'))
     return render_template('new_donation.jinja2')
 
+
+@app.route('/individual-donations', methods=['GET', 'POST'])
+def indiv_donation():
+    if request.method == 'POST':
+        donor_name = request.form['donor-name']
+        if donor_name != 'all':
+            try:
+                retrieved_donor = Donor.select().where(Donor.name == donor_name).get()
+                all_donations = Donation.select().where(Donation.donor == retrieved_donor)
+
+                return render_template('individual_donations.jinja2', donor_name=donor_name, all_donations=all_donations, post=True)
+            except:
+                return render_template('individual_donations.jinja2', donor_name=donor_name, post=True)
+
+    all_donors = Donor.select()
+    return render_template('individual_donations.jinja2', all_donors=all_donors)
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
